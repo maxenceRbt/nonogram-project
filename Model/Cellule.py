59a968f2188faecefa1decd59f1d24585485bce8
rabo0006 <maxence.rabotin@etudiant.univ-reims.fr>
2026-01-06 11:29:16 +0100

    Implémentation des cellules : tests OK

--- Model/Cellule.py
+++ Model/Cellule.py
@@ -50,3 +50,69 @@ def construireCellule() -> Cellule:
     """
     Cellule = {VALEUR: None}
     return Cellule
+
+def isVuCellule(cell: Cellule) -> bool:
+    """
+    Fonction permettant de vérifier si la cellule est bien découverte
+
+    :param cell: cellule concernée
+    :return: True s'il s'agit d'une cellule découverte, False sinon.
+    :raise AssertionError: Si le paramètre n'est pas une cellule
+    """
+    test = False
+    assert type_cellule(cell) == True, "La cellule n'est pas une cellule"
+    if cell[VALEUR] != None:
+        test = True
+    return test
+
+def isVideCellule(cell: Cellule) -> bool:
+    """
+    Fonction permettant de vérifier si la cellule est vide
+
+    :param cell: cellule concernée
+    :return: True si la cellule est vide, False sinon
+    :raise AssertionError: Si le paramètre n'est pas une cellule
+    """
+    test = False
+    assert type_cellule(cell) == True, "La cellule n'est pas une cellule"
+    if cell[VALEUR] == 0:
+        test = True
+    return test
+
+def getCouleurCellule(cell: Cellule) -> int:
+    """
+    Fonction permettant d'obtenir la valeur de la couleur de la cellule
+
+    :param cell: cellule concernée
+    :return: l'entier représentant la couleur de la cellule
+    :raise AssertionError: Si le paramètre n'est pas une cellule ou si la cellule ne contient pas de couleur
+    """
+    assert type_cellule(cell) == True and cell[VALEUR] >= 0, "La cellule n'est pas une cellule ou n'a pas de couleur "
+    couleur = cell[VALEUR]
+    return couleur
+
+def setCouleurCellule(cell: Cellule, couleur: int) -> None:
+    """
+    Fonction permettant de définir une couleur à une cellule
+
+    :param cell: cellule concernée
+    :param couleur: couleur attribuée à la cellule concernée
+    :return: None
+    :raise AssertionError: Si le paramètre n'est pas une cellule ou que le second paramètre n'est pas un entier
+    """
+    assert type_cellule(cell) == True and type(couleur) == int, "La cellule n'est pas une cellule ou le second paramètre n'est pas un entier"
+    cell[VALEUR] = couleur
+    return None
+
+def setNonVuCellule(cell : Cellule) -> None:
+    """
+    Fonction permettant de rendre une cellule en non-"découverte"
+
+    :param cell: Cellule concernée
+    :return: None
+    :raise AssertionError: Si le paramètre n'est pas une cellule
+    """
+    assert type_cellule(cell) == True, "La cellule n'est pas une cellule"
+    cell[VALEUR] = None
+    return None
+
