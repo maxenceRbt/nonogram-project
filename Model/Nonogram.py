a86e42b4e31d1df813e7a7aa71103d931d602c14
rabo0006 <maxence.rabotin@etudiant.univ-reims.fr>
2026-01-07 16:11:03 +0100

    Comptage des blocs sur les colonnes : tests ok

--- Model/Nonogram.py
+++ Model/Nonogram.py
@@ -37,7 +37,7 @@ def getBlocsLignes(image: ModelImage) -> LstBlocs:
     Fonction permettant d'obtenir les blocs pour chaque ligne d'une image donnée
 
     :param image: image concernée
-    :return: une list de liste des blocs pour chaque ligne
+    :return: une liste de liste des blocs pour chaque ligne
     :raise AssertionError: Si le paramètre n'est pas une image
     """
     assert type_image_light(image) == True, "le paramètre n'est pas une image"
@@ -47,3 +47,44 @@ def getBlocsLignes(image: ModelImage) -> LstBlocs:
         blocs.append(blocLigne)
     return blocs
 
+def compterBlocsSurColonne(image: ModelImage, colonne: int = 0) -> LstBlocs:
+    """
+    Fonction permettant de compter les blocs pour une colonne donnée, d'une image donnée
+
+    :param image: image concernée
+    :param colonne: colonne choisit
+    :return: une liste des blocs sur la colonne
+    :raise AssertionError: Si le premier paramètre n'est pas une image ou si le second n'est pas un entier
+    """
+    assert type_image_light(image) == True, "le premier paramètre n'est pas une image"
+    assert type(colonne) == int, "le second paramètre n'est pas un entier"
+    listeBlocs = []
+    couleur = getCouleurCellule(image[0][colonne])
+    longueur = 1
+    for i in range(1, len(image)):
+        couleurCell = getCouleurCellule(image[i][colonne])
+        if couleurCell != couleur:
+            if couleur != 0:
+                listeBlocs.append(construireBloc(longueur, couleur))
+            couleur = couleurCell
+            longueur = 1
+        else:
+            if couleur != 0:
+                longueur += 1
+    if couleur != 0:
+        listeBlocs.append(construireBloc(longueur, couleur))
+    return listeBlocs
+
+def getBlocsColonnes(image: ModelImage) -> LstBlocs:
+    """
+    Fonction permettant de compter les blocs sur chaque colonne d'une image donnée
+
+    :param image: image concernée
+    :return: Une liste de liste des blocs pour chaque colonne
+    """
+    assert type_image_light(image) == True, "Le paramètre n'est pas une image"
+    blocs = []
+    for j in range(len(image[0])):
+        blocsColonne = compterBlocsSurColonne(image, j)
+        blocs.append(blocsColonne)
+    return blocs
